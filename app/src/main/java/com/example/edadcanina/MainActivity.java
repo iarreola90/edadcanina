5d498173371840b7c28c557979c609cd63103982
ivan <ivanarreolaruelas@gmail.com>
2024-09-18 20:11:48 -0600

    Se calcula la edad del lomito

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        EditText edadedit = findViewById(R.id.edad_edit);
+        TextView resultexto = findViewById(R.id.resultado_texto);
+        Button boton = findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadedit.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint * 7;
+                String resultadoString = "La edad de tu perro es: " + resultado + " años";
+                resultexto.setText(resultadoString);
+            }
+        });
     }
 }
